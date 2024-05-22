3f323b7611e884da8749566f0f363e4c34160034
Jacob Allen <47609209+jallen2034@users.noreply.github.com>
2024-05-21 19:20:13 -0700

    Create maxLenSubString.js

---
+++ maxLenSubString.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const maximumLengthSubstring = function (s) {
+  let left = 0
+  let arrLen = 0
+  const resultHash = new Map()
+
+  for (let right = 0; right < s.length; right++) {
+    let value = s[right]
+
+    while (resultHash.get(value) === 2) {
+      resultHash.set(s[left], resultHash.get(s[left]) - 1)
+      left += 1
+    }
+
+    resultHash.set(value, (resultHash.has(value) ? resultHash.get(value) : 0) + 1)
+    arrLen = Math.max(arrLen, right-left+1);
+  }
+
+  return arrLen
+}
+
+console.log(maximumLengthSubstring("bcbbbcba")) // output 4
+console.log(maximumLengthSubstring("aaaa")) // 2
